4e7a58b7d17170b91faae5e713f49bb486c98ed9
admin <admin@admins-MacBook-Pro.local>
2017-01-21 13:30:32 +0100

    hw1 bonus task

--- src/main/java/zhenya/workshop_1/Tasks.java
+++ src/main/java/zhenya/workshop_1/Tasks.java
@@ -2,12 +2,7 @@ package zhenya.workshop_1;
 
 public class Tasks {
 
-    public static void main(String[] args) {
-        displayResultsOfLogicalOperations(true, true);
-        displayResultsOfLogicalOperations(false, true);
-        displayResultsOfLogicalOperations(true, false);
-        displayResultsOfLogicalOperations(false, false);
-    }
+
 
     public static String getIntAsString(int x) {
         x = 12;
@@ -54,9 +49,6 @@ public class Tasks {
     // NOT argument #1 = false
     //  NOT argument #2 = false
 
-    //test new commint
-
-
     public static void displayResultsOfLogicalOperations(boolean a, boolean b) {
         System.out.println(String.format("argument #1 = %s, argument #2 = %s", a, b));
         System.out.println("argument #1 OR argument #2 = " + (a || b));
@@ -64,5 +56,36 @@ public class Tasks {
         System.out.println("argument #1 ^ argument #2 = " + (a ^ b));
         System.out.println("NOT argument #1 = " + !a);
         System.out.println("NOT argument #2 = " + !b);
+    }
+
+    public static void main(String[] args) {
+
+        //task4
+        displayResultsOfLogicalOperations(true, true);
+        displayResultsOfLogicalOperations(false, true);
+        displayResultsOfLogicalOperations(true, false);
+        displayResultsOfLogicalOperations(false, false);
+
+        //taskBonus
+        double a = 20;
+        double b = 80;
+        reminderLimit(a, b);
+    }
+
+    // BONUS
+    // 1. Create a double variable with the value 20
+    // 2. Create a second variable of type double with the value 80
+    // 3. Add both numbers up and multiply by 25
+    // 4. Use the reminder operator to figure out the reminder from the sum of #3 divided by 40
+    // 5. Write an "if" statement that displays a message "Total was over the limit"
+    //  if the remaining total (#4) is equal to 20 or less
+
+    public static void reminderLimit(double a, double b){
+        System.out.println("*Bonus task*");
+        double sum = (a + b) * 25;
+        double reminder = sum % 40;
+        if (reminder <= 20){
+            System.out.println("Total was over the limit");
+        }
     }
 }
