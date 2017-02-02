2faa86b2c546de4277d79633fb7e1e68affe978d
admin <admin@admins-MacBook-Pro.local>
2017-02-02 21:59:47 +0100

    task1, task2

---
+++ src/main/java/zhenya/workshop_3/Task1.java
@@ -0,0 +1,51 @@
+package zhenya.workshop_3;
+
+public class Task1 {
+
+    public static String buttonEmail = "Sign in via email";
+    public static String buttonFB = "Sign in via Facebook";
+    public static String buttonGoogle = "Sign in via Google Plus";
+
+    private static void clickEmail(){
+        System.out.println("You have pressed \"Sign in via email\". Wait for redirection to your account page");
+    }
+
+    private static void clickFB(){
+        System.out.println("You have pressed \"Sign in via Facebook\". Wait for redirection to Facebook");
+    }
+
+    private static void clickGoogle(){
+        System.out.println("You have pressed \"Sign in via Google Plus\". Wait for redirection to Google");
+    }
+
+
+    private static void clickUnknown(){
+        System.out.println("You have pressed unknown element and system error occurred. Please, contact site administrator");
+    }
+
+    private static void analyzeRedirect(String buttonName) {
+        switch (buttonName) {
+            case "Sign in via email":
+                clickEmail();
+                break;
+            case "Sign in via Facebook":
+                clickFB();
+                break;
+            case "Sign in via Google Plus":
+                clickGoogle();
+                break;
+            default:
+                clickUnknown();
+                break;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        analyzeRedirect(buttonEmail);
+        analyzeRedirect(buttonFB);
+        analyzeRedirect(buttonGoogle);
+        analyzeRedirect("Lorem Ipsum");
+    }
+
+}
---
+++ src/main/java/zhenya/workshop_3/Task2.java
@@ -0,0 +1,36 @@
+package zhenya.workshop_3;
+
+public class Task2 {
+
+    private static void printTheLongestWord(String sentence){
+        if (sentence == ""){
+            System.out.println("In the sentence:\n" +
+                    "\"\"\n" +
+                    "The longest word cannot be calculated, because there is no sentence \n");
+        } else {
+            String[] words=sentence.replaceAll("[\\p{Punct}&&[^']]","").split("\\s+");
+            String longestWord = words[0];
+            for (int i = 1; i< words.length; i++){
+                if (longestWord.length() <= words[i].length()){
+                    longestWord = words[i];
+                    }
+                }
+                System.out.println("In the sentence:\n" +
+                        "\"" + sentence + "\"\n" +
+                        "The longest word is \"" + longestWord + "\"\n");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        printTheLongestWord("Buy yellow banana!");
+        printTheLongestWord("");
+        printTheLongestWord("I don't need a hair stylist, my pillow gives me a new hairstyle every morning.");
+        printTheLongestWord("The quick brown fox jumps over the lazy dog");
+        printTheLongestWord("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        printTheLongestWord("don't.");
+    }
+
+}
+
+
