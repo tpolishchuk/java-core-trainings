e97cd1dda1622e26a580c92d418fc4f324cf04ee
serhatgemici <serhat.gemici@carmudi.com>
2017-02-14 12:37:12 +0100

    Update Task1.java
    added smaller than equal to 200 and 500

--- src/main/java/serhat/workshop_2/Task1.java
+++ src/main/java/serhat/workshop_2/Task1.java
@@ -12,9 +12,9 @@ public class Task1 {
     private static void analyzeGivenIntValue(int valueToBeAnalyzed) {
         if (valueToBeAnalyzed <= 100)
             System.out.println("Given argument value is less or equals 100");
-        else if ((valueToBeAnalyzed > 100) && (valueToBeAnalyzed < 200))
+        else if ((valueToBeAnalyzed > 100) && (valueToBeAnalyzed <= 200))
             System.out.println("Given argument value is between 100 and 200");
-        else if ((valueToBeAnalyzed > 200) && (valueToBeAnalyzed < 500))
+        else if ((valueToBeAnalyzed > 200) && (valueToBeAnalyzed <= 500))
             System.out.println("Given argument value is between 200 and 500");
         else if (valueToBeAnalyzed > 500)
             System.out.println("Given argument value is more 500");
