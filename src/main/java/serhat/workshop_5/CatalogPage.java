fe4d2f1a5a908b8b5a55275d3f54601fe469feb5
serhatgemici <serhat.gemici@carmudi.com>
2017-04-05 14:50:05 +0200

    == operators changed to .equals()

--- src/main/java/serhat/workshop_5/CatalogPage.java
+++ src/main/java/serhat/workshop_5/CatalogPage.java
@@ -5,38 +5,31 @@ package serhat.workshop_5;
  */
 public class CatalogPage extends BasePage {
 
-
     protected CatalogPage(String partialURL) {
         super(partialURL);
     }
 
     @Override
     protected void openPage() {
-
-        if (!partialURL.equals("/cars") && !partialURL.equals("/motorcycles") && !partialURL.equals("/commercial-vehicles") && !partialURL.equals("/all")) {
-            System.out.println("Opening URL: " + BASE_URL + partialURL);
-            System.out.println("Error: Cannot handle given partial url /sellcar, page will not be shown");
-        }
-
-        if (partialURL == "/cars") {
+       if (partialURL.equals("/cars")) {
             System.out.println("Opening URL: " + BASE_URL + partialURL);
             showHeader();
             System.out.println("cars catalog is shown");
             showFooter();
             System.out.println("***\n");
-        } else if (partialURL == "/motorcycles") {
+        } else if (partialURL.equals("/motorcycles")) {
             System.out.println("Opening URL: " + BASE_URL + partialURL);
             showHeader();
             System.out.println("motorcycles catalog is shown");
             showFooter();
             System.out.println("***\n");
-        } else if (partialURL == "/commercial-vehicles") {
+        } else if (partialURL.equals("/commercial-vehicles")) {
             System.out.println("Opening URL: " + BASE_URL + partialURL);
             showHeader();
             System.out.println("commercial-vehicles catalog is shown");
             showFooter();
             System.out.println("***\n");
-        } else if (partialURL == "/all") {
+        } else if (partialURL.equals("/all")) {
             System.out.println("Opening URL: " + BASE_URL + partialURL);
             showHeader();
             System.out.println("All Vehicles catalog is shown");
