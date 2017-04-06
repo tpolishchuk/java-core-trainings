18d3540d5c97bb33300ea4efbfb4d41b52032a9d
Evgeniia Balyasina <evgeniia.balyasina@carmudi.com>
2017-04-06 07:37:10 +0200

    workhop7.task1 refactoring

--- src/main/java/zhenya/workshop_7/RandomIntegersGenerator.java
+++ src/main/java/zhenya/workshop_7/RandomIntegersGenerator.java
@@ -2,13 +2,13 @@ package zhenya.workshop_7;
 
 public class RandomIntegersGenerator extends RandomValuesGenerator {
 
-    public static int generateInt() {
+    protected static int generateInt() {
         return (int) (Math.random() * 100 + 1);
     }
 
     @Override
     public void generateRandomStuff(int objectsAmount){
-        int randomNumber = 0;
+        int randomNumber;
         for (int i=1; i<=objectsAmount; i++){
             randomNumber = generateInt();
             System.out.println("Random number #" + i + ": " + randomNumber);
--- src/main/java/zhenya/workshop_7/RandomStringsGenerator.java
+++ src/main/java/zhenya/workshop_7/RandomStringsGenerator.java
@@ -4,13 +4,13 @@ import org.apache.commons.lang.RandomStringUtils;
 
 public class RandomStringsGenerator extends RandomValuesGenerator {
 
-    public static String generateString(int count) {
+    protected static String generateString(int count) {
         return RandomStringUtils.randomAlphabetic(count);
     }
 
     @Override
     public void generateRandomStuff(int objectsAmount) {
-        String randomString = "";
+        String randomString;
         for (int i = 1; i <= objectsAmount; i++) {
             randomString = generateString(10);
             System.out.println("Random string #" + i + ": " + randomString);
