bddadad493796898ce77c443094a0a6202e6e53e
Evgeniia Balyasina <evgeniia.balyasina@carmudi.com>
2017-04-08 13:51:03 +0200

    workhop7.task2 equals fix

--- src/main/java/zhenya/workshop_7/Table.java
+++ src/main/java/zhenya/workshop_7/Table.java
@@ -26,7 +26,7 @@ public class Table {
 
     @Override
     public boolean equals(Object o) {
-        if ((o instanceof Table) && ((Table) o).getPrice() == this.price && ((Table) o).getTitle() == this.title) {
+        if ((o instanceof Table) && ((Table) o).getPrice() == this.price && ((Table) o).getTitle().equals(this.title)) {
             return true;
         }
         else {
