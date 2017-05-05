1618c7be577c12d4102bf0a8dba4d9e9dd99be2b
Evgeniia Balyasina <evgeniia.balyasina@carmudi.com>
2017-05-05 14:15:50 +0200

    hw10.Task1

--- src/main/java/zhenya/workshop_10/Task1.java
+++ src/main/java/zhenya/workshop_10/Task1.java
@@ -1,4 +1,33 @@
 package zhenya.workshop_10;
 
+import java.util.ArrayList;
+
 public class Task1 {
+
+    private static final int RANGE = 10;
+
+    public static void main(String[] args) {
+        int[] initialArray = generateArray(RANGE);
+
+        System.out.println("Converted array " + convertArray(initialArray));
+    }
+
+    private static int[] generateArray(int range) {
+        int[] generatedArray = new int[range];
+
+        for (int i = 0; i < range; i++) {
+            generatedArray[i] = (int) (Math.random() * 100 + 1);
+        }
+
+        return generatedArray;
+    }
+
+    private static ArrayList<Integer> convertArray(int[] initialArray) {
+        ArrayList<Integer> convertedArray = new ArrayList<>();
+
+        for (int i = 0; i < initialArray.length; i++) {
+            convertedArray.add(i, initialArray[i]);
+        }
+        return convertedArray;
+    }
 }
